18f1898a1722edd322f379910ab2dc32c8854e17
ImaiShota <102264809+imaishota0611@users.noreply.github.com>
2022-05-09 08:35:24 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import "./styles.css";
+import axios from "axios";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { User } from "./types/user";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+
+const user: User = {
+  name: "imai",
+  hobbies: ["ゲーム", "格闘技観戦"]
+};
+
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickFetchData = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+  return (
+    <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
+      <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
